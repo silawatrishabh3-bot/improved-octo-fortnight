26737bfa64535a313155091f671ccf13e7508312
Rishabh Silawat <silawatrishabh3@gmail.com>
2026-09-13 16:33:30 +0530

    fix render server port binding

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,13 @@
 import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 
-//connections and listeneres
+// connections and listeners
 const PORT = process.env.PORT || 5000;
+
 connectToDatabase()
   .then(() => {
-    app.listen(PORT, () =>
-      console.log("Server Open & Connected To Database <3")
-    );
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((err) => console.log(err));
